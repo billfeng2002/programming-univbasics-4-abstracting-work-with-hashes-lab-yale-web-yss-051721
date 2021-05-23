b4d1d08a09a8f591cf1e902369ac2f6a467d75c6
Bill Feng <bill.feng@yale.edu>
2021-05-23 06:10:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,8 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  
+  return hash.fetch(key)
+  return hash[key]
 end
 
 def update_counting_hash(hash, key)
